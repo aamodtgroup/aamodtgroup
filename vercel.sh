ae173287782570fb098090ded10cd2d0f6cb63af
Kasper Aamodt <44305520+kasperaamodt@users.noreply.github.com>
2021-05-27 10:55:01 +0200

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-if [[ $VERCEL_GIT_COMMIT_REF != dependabot/* ]]; then 
+if [[ $VERCEL_GIT_COMMIT_REF != ~ "dependabot" ]]; then
+  echo "Deploying"
   exit 1; 
+else
+  echo "This is a dependabot branch, cancel preview"
+  exit 0;
 fi
-exit 0;
