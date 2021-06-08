172eda53141fba4e459a06351b29f6226a946b69
Kasper Aamodt <kasper@aamodt.xyz>
2021-06-08 14:54:03 +0200

    Update header.js

--- packages/agtech/src/components/header/header.js
+++ packages/agtech/src/components/header/header.js
@@ -54,8 +54,24 @@ const HeaderContainer = styled.div`
     z-index: 999;
     height: 5px;
     width: 100vw;
-    background-image: linear-gradient(
-      90deg
-      ,red,#ffb921 20%,#36bc9b 40%,#5c8fc9 60%,#a88ad8 80%,#891c96 100%,#920389);
+    background: linear-gradient(
+      90deg,
+      #000,
+      #000 12.5%,
+      #784f16 12.5%,
+      #784f16 25%,
+      #e40303 25%,
+      #e40303 37.5%,
+      #ff8c00 37.5%,
+      #ff8c00 50%,
+      #ffed00 50%,
+      #ffed00 62.5%,
+      #008026 62.5%,
+      #008026 75%,
+      #004dff 75%,
+      #004dff 87.5%,
+      #750787 87.5%,
+      #750787
+    );
   }
 `;
